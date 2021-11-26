ba9508fdba9dec9baf2ba1db6f2692c2423447c9
yianzaratin <yian@uol.com.br>
2021-11-26 08:44:26 -0300

    Project Created

---
+++ src/test/java/com/digitalinnovationone/heroesapi/HeroesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.heroesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
